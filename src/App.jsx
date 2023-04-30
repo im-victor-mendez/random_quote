5591373fc10b0be39e5b5e60eeabbb6132f1f119
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-04-29 21:07:10 -0600

    feat: Implemented basic structure
    This implements:
        - freecodecamp test
        - QuoteBox layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import QuoteBox from "./layouts/QuoteBox/QuoteBox"
+
 function App() {
   return (
-    <>
-    </>
+    <section id="App">
+      <QuoteBox />
+    </section>
   )
 }
 
---
+++ src/layouts/QuoteBox/QuoteBox.jsx
@@ -0,0 +1,14 @@
+//import React from 'react'
+
+function QuoteBox() {
+  return (
+    <section id='quote-box'>
+        <p id='text'></p>
+        <h1 id='author'></h1>
+        <button id="new-quote"></button>
+        <a id="tweet-quote" href="http://" target="_blank" rel="noopener noreferrer"></a>
+    </section>
+  )
+}
+
+export default QuoteBox
